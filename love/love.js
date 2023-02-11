fabeab47c53ff39d5a1045d3c6717bb5deb36328
monsur776e <monsuralam8897@gmail.com>
2023-02-11 03:49:29 -0500

    add

---
+++ love/love.js
@@ -0,0 +1,15 @@
+function yesDo(){
+    document.body.style.backgroundColor = 'yellow'
+    document.getElementById('h1').innerText = 'Aww babes Love you so much...!'
+    document.getElementById('h1').style.color = 'blue'
+    
+//    const img = document.createElement('img');
+//    img.setAttribute('src','couple-1643452.jpg')
+//    document.body.appendChild(img)
+        
+}
+
+function nono(){
+    document.getElementById('h1').innerText = 'Why You broke my Heart babes ..>:'
+    document.getElementById('h1').style.color = 'black'
+}
